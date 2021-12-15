26c005d92ba948998282d42eef0e13b8e953c866
ChuaShuErn <shuern.chua@gmail.com>
2021-12-16 07:16:53 +0800

    Removed Spring Security Dependency which caused it to use Spring Security Login. Now the landing page is index.html

--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/User.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/User.java
@@ -9,9 +9,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
