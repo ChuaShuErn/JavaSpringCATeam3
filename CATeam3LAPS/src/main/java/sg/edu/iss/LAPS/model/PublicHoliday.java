f56b99a8c98142fb1a6a7dca5bc3a468cef7d70d
manuelseet <manuelseet@gmail.com>
2021-12-17 12:27:57 +0800

    added public holiday model

---
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/PublicHoliday.java
@@ -0,0 +1,29 @@
+package sg.edu.iss.LAPS.model;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PublicHolidays")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublicHoliday {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer holidayId;
+    private String holidayName;
+    private Date holidayStartDate;
+    private Date holidayEndDate;
+
+}
