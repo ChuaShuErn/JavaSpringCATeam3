40e5a6800d4807601da83bf788fc3e9ae402145e
XueRong <xuerongzhang77@gmail.com>
2021-12-19 01:38:09 +0800

    add service and repository for leave application

---
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/services/ApplyLeaveService.java
@@ -0,0 +1,9 @@
+package sg.edu.iss.LAPS.services;
+
+import sg.edu.iss.LAPS.model.LeaveApplied;
+
+public interface ApplyLeaveService {
+	
+	public void createLeaveApplication(LeaveApplied leaveApplied); // To add a new leave application for user.
+	
+}
