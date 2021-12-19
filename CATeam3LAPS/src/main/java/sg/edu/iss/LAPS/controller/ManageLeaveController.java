e98f7fbcd7420b7bda8abb06e192e7da83cf8a55
Yu Jing <e0703487@u.nus.edu>
2021-12-19 17:44:13 +0800

    Add current leaves page

--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/controller/ManageLeaveController.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/controller/ManageLeaveController.java
@@ -1,16 +1,13 @@
 package sg.edu.iss.LAPS.controller;
 
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import sg.edu.iss.LAPS.model.ApprovalStatus;
-import sg.edu.iss.LAPS.model.LeaveApplied;
 import sg.edu.iss.LAPS.services.LeaveAppliedService;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 @RequestMapping("/leave")
@@ -19,7 +16,7 @@ public class ManageLeaveController {
     LeaveAppliedService service;
 
     @RequestMapping(value = "/viewHistory")
-    public String viewMyLeaveHistory(@RequestParam(name = "status", required = false) String status, Model model, HttpSession session) {
+    public String viewMyLeaveHistory(Model model, HttpSession session) {
 //        int id = (int) session.getAttribute("id");
 //        List<LeaveApplication> leaves = lservice.findApplicationByStaffId(id);
 //        model.addAttribute("leaves", leaves);
@@ -32,20 +29,15 @@ public class ManageLeaveController {
 
 //        return "viewHistory";
         Long userId = 1L;
-        List<LeaveApplied> leaveAppliedList;
-        if (!Strings.isBlank(status)) {
-            leaveAppliedList = service.findByUserId(userId, ApprovalStatus.valueOf(status));
-        } else {
-            leaveAppliedList = service.findByUserId(userId);
-        }
-
-        model.addAttribute(("LeaveApplied"), leaveAppliedList);
+        model.addAttribute("leaveAppliedList", service.findByUserId(userId));
         return "viewHistory";
     }
 
-//    @RequestMapping(value = "updateLeaveApplied")
-//    public void update(@ModelAttribute("leaveapplication") LeaveApplication leaveApp) {
-//
-//    }
+    @RequestMapping(value = "/currentLeaves")
+    public String viewCurrentLeaves(@RequestParam(name = "status") String status, Model model, HttpSession session) {
+        Long userId = 1L;
+        model.addAttribute("leaveAppliedList", service.findByUserId(userId, ApprovalStatus.valueOf(status)));
+        return "currentLeaves";
+    }
 
 }
