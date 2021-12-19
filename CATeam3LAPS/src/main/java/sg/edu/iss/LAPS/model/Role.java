ffe86e199a05deb67fe7832bb3f7a32674bcea3f
Manasjyoti Borah <manasborah90@gmail.com>
2021-12-19 16:57:45 +0800

    Role added

--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/Role.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/Role.java
@@ -19,7 +19,7 @@ public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="role_id")
-	private Integer id;
+	private Long id;
 	
 	@Column(name="role_name", unique=true, nullable=false)
 	private String name;
--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/repo/RoleRepository.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/repo/RoleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import sg.edu.iss.LAPS.model.Role;
 
-public interface RoleRepository extends JpaRepository<Role, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
