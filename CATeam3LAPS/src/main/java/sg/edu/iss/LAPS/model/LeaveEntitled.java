046667514383b958164a6e072fe16874831b4b2f
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-12-21 20:19:56 +0800

    -Revert back the leave entitlement table

--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/LeaveEntitled.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/LeaveEntitled.java
@@ -1,24 +1,16 @@
 package sg.edu.iss.LAPS.model;
-import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import sg.edu.iss.LAPS.utility.ClaimStatus;
 
 @Entity
 @Data
@@ -39,11 +31,4 @@ public class LeaveEntitled {
     public String toString(){
         return leaveEntitledId + "  " + totalLeave;
     }
-    
-    @Temporal(TemporalType.DATE)
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    public Date claimDate;
-    
-    @Enumerated(EnumType.STRING)
-    public ClaimStatus claimStatus;
 }
