ffa8fe29f21a8769e9ae71efe2f85ac54e0580c3
ChuaShuErn <shuern.chua@gmail.com>
2021-12-22 11:40:35 +0800

    functional employee compensation claim. Manager approval in progress

---
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/services/ClaimCompensationService.java
@@ -0,0 +1,18 @@
+package sg.edu.iss.LAPS.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sg.edu.iss.LAPS.model.ClaimCompensation;
+import sg.edu.iss.LAPS.model.LeaveApplied;
+
+
+public interface ClaimCompensationService {
+	
+	public void createCompensationClaim(ClaimCompensation claimCompensation); 
+	
+	//ArrayList<ClaimCompensation> findClaimsListByUserId(Long userID);
+	
+	public ArrayList<ClaimCompensation>findByUserId(Long userId);
+
+}
