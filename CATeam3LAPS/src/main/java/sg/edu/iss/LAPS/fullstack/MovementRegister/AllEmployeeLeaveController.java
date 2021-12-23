c780268216060d116cc53ec891e5285a098e51db
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2021-12-23 12:23:28 +0800

    -Update the fullstack file

--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/fullstack/MovementRegister/AllEmployeeLeaveController.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/fullstack/MovementRegister/AllEmployeeLeaveController.java
@@ -2,11 +2,14 @@ package sg.edu.iss.LAPS.fullstack.MovementRegister;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +24,37 @@ public class AllEmployeeLeaveController {
 	LeaveAppliedRepository leaveAppliedRepository;
 	
 	
-	@GetMapping("/LeaveApplied")
+	@GetMapping("/leaveApplied")
 	public List<LeaveApplied> getLeaveApplied(){
 		LocalDate currentdate = LocalDate.now();
 		int currentMonth=currentdate.getMonthValue();
-		return leaveAppliedRepository.findLeaveAppliedByMonth(currentMonth);
+		int currentYear=currentdate.getYear();
+		List<LeaveApplied> leaveList=leaveAppliedRepository.findByMonth(currentMonth,currentYear);
+		leaveList.stream().forEach(x->{
+			x.getUser().setLeaveAppliedList(null);
+			x.getUser().setLeaveEntitledList(null);});
+		return leaveList;
 	}
 	
-	
+	@PostMapping("/leaveApplied/update/{month}")
+	public ResponseEntity<?> updateMonth(@PathVariable("month") int month) {
+		LocalDate currentdate = LocalDate.now();
+		int yearToUpdate=currentdate.getYear();
+		if (month>12)
+		{
+			yearToUpdate++;
+			month-=12;
+		}
+		List<LeaveApplied> newList = leaveAppliedRepository.findByMonth(month,yearToUpdate);
+		newList.stream().forEach(x->{
+			x.getUser().setLeaveAppliedList(null);
+			x.getUser().setLeaveEntitledList(null);});
+		if (!newList.isEmpty()) {
+			return new ResponseEntity<>(newList, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 	
 	
 }
--- CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/LeaveApplied.java
+++ CATeam3LAPS/src/main/java/sg/edu/iss/LAPS/model/LeaveApplied.java
@@ -1,15 +1,27 @@
 package sg.edu.iss.LAPS.model;
 
+import java.time.LocalTime;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import sg.edu.iss.LAPS.utility.LeaveStatus;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.time.LocalTime;
-import java.util.Date;
 
 @Entity
 @Table(name = "LeaveApplied")
@@ -48,7 +60,7 @@ public class LeaveApplied {
     private LeaveStatus approvalStatus;
 
     private String managerComments;
-
+    
     @ManyToOne
     private User user;
 
